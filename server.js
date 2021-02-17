41b507bf99f11a87fd22a36e032cfbd2e2f150d7
ssong192 <ssong192@greencatsoft.com>
2021-02-17 16:49:59 +0900

    커밋!

--- server.js
+++ server.js
@@ -6,6 +6,14 @@ app.set('views', __dirname + '/views');
 app.set('view engine', 'ejs');
 app.engine('html', require('ejs').renderFile);
 
+var requestTime = function (req, res, next) {
+    req.requestTime = Date.now();
+    console.log(Date.now());
+    next();
+};
+
+app.use(requestTime);
+
 const server = app.listen(3000, function(){
     console.log("Express server has started on port 3000")
 });
